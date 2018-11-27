e0146783240383be49997ec3e464fa40c052fbd5
Semyon Bochkaryov <squizduos@gmail.com>
2018-11-28 02:28:37 +0300

    1

--- imgdownloader/settings.py
+++ imgdownloader/settings.py
@@ -125,6 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'production_static')

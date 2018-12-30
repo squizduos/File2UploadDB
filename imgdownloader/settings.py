f9cde16ce89a3c301f1cc45eb2c2682a133af07b
Semyon Bochkaryov <squizduos@gmail.com>
2018-12-30 11:55:58 +0300

    1

--- imgdownloader/settings.py
+++ imgdownloader/settings.py
@@ -136,7 +136,7 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'production_static')
 
-DOMAIN = "img-test.squizduos.ru"
+DOMAIN = os.getenv("VIRTUAL_HOST", "localhost")
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.udag.de'

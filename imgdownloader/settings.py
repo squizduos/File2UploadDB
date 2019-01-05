33db27e642495b7daf4bf5c599a98e8384da16eb
Semyon Bochkaryov <squizduos@gmail.com>
2019-01-05 09:07:57 +0300

    1

--- imgdownloader/settings.py
+++ imgdownloader/settings.py
@@ -80,8 +80,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
     'EXCEPTION_HANDLER': 'imgdownloader.exceptions.custom_exception_handler'
